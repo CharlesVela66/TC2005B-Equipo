41b37038d9b4a2deec253c8c186e37a8ba5d3942
Carlos Velasco <A01708634@tec.mx>
2023-03-29 17:29:41 -0600

    Correccion de Registrar Bitacora

--- models/bitacora.model.js
+++ models/bitacora.model.js
@@ -5,18 +5,19 @@ module.exports = class Bitacora {
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
     constructor(nuevo_registro) {
         this.id_cliente = nuevo_registro.id_cliente;
-        this.id_rutina = nuevo_registro.id_rutina;
-        this.descr_sesion = nuevo_registro.descr_sesion || null;
-        this.nivel_satisf = nuevo_registro.nivel_satisf || 1;
+        this.id_rutina = nuevo_registro.id_rutina || 1;
+        this.fecha = nuevo_registro.fecha;
+        this.descr_sesion = nuevo_registro.descr_sesion;
+        this.nivel_satisf = nuevo_registro.nivel_satisf || null;
         this.comentarios = nuevo_registro.comentarios || "No agregó comentarios";
     }
 
     //Este método servirá para guardar de manera persistente el nuevo objeto. 
     save() {
         return db.execute(`
-            INSERT INTO bitacora (id_cliente, id_rutina, nivel_satisf, descripcion_sesion, comentarios)
-            VALUES (?, ?, ?, ?, ?)
-        `, [this.id_cliente, 1, this.nivel_satisf, this.descr_sesion, this.comentarios]);
+            INSERT INTO bitacora (id_cliente, id_rutina, fecha, nivel_satisf, descripcion_sesion, comentarios)
+            VALUES (?, ?, ?, ?, ?, ?)
+        `, [this.id_cliente, this.id_rutina, this.fecha, this.nivel_satisf, this.descr_sesion, this.comentarios]);
     }
 
     //Este método servirá para devolver los objetos del almacenamiento persistente.
