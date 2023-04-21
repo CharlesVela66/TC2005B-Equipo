18b3e001ff3d23a883f5a7459ed61d37f03fb25a
Carlos Velasco <A01708634@tec.mx>
2023-04-21 07:35:12 -0600

    Agregar restricciones para rutas unicas de Admin

--- routes/perfil.routes.js
+++ routes/perfil.routes.js
@@ -2,15 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
+const hasCreate = require('../util/has-create');
+
 const perfilController = require('../controllers/perfil.controller');
 
 router.get('/', perfilController.ver_perfil);
 
 router.get('/ver_info', perfilController.verCliente);
 
-router.get('/ver-info', perfilController.verAdministrador);
+router.get('/ver-info', hasCreate, perfilController.verAdministrador);
 
 router.get('/editar/perfil', perfilController.get_editarPerfil);
+
 router.post('/editar/perfil', perfilController.post_editarPerfil);
 
 module.exports = router;
